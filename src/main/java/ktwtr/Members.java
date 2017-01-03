35c3027eb7eb5c3c75512427e5987bb359b8be2a
rhidja <rhidja@gmail.com>
2017-01-04 00:04:52 +0100

    cleanup

--- src/main/java/ktwtr/Members.java
+++ src/main/java/ktwtr/Members.java
@@ -3,7 +3,6 @@ package ktwtr;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
